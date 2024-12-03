211f60941d4651544a6e91c6f842f707ed0b9ca9
zandalman <zandalman@gmail.com>
2024-12-02 22:52:01 -0500

    finished assignment

--- src/_core.cpp
+++ src/_core.cpp
@@ -7,5 +7,10 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
-  /// ... Fill me in!
+  m.doc() = "Python bindings for se_for_sci_hw6 C++ library";
+  py::class_<HarmonicOscillator>(m, "HarmonicOscillator")
+    .def(py::init<>())
+    .def("f", &HarmonicOscillator::f, "Harmonic oscillator ODE")
+    .def("compute_step", &HarmonicOscillator::compute_step, "Compute an integration step")
+    .def("integrate", &HarmonicOscillator::integrate, "Integrate the ODE");
 }
